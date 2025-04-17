b4ac283c2614d816a7bf789645b170809d1e54e6
MostafaDiab <mostafa3diab55@gmail.com>
2025-04-17 23:04:39 +0200

    added some files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainDashboard from "./components/MainDashboard/MainDashboard";
+import NFTMarketplace from "./components/NFTMarketplace/NFTMarketplace";
+import DataTables from "./components/DataTables/DataTables";
+import Profile from "./components/Profile/Profile";
+import SignIn from "./components/SignIn/signIn";
+import Layout from "./components/Layout/Layout";
+import Notfound from "./components/Notfound/Notfound";
+
+function App() {
+  let routers = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout />,
+      children: [
+        { index: true, element: <MainDashboard /> },
+        { path: "DataTables", element: <DataTables /> },
+        { path: "MainDashboard", element: <MainDashboard /> },
+        { path: "NFTMarketplace", element: <NFTMarketplace /> },
+        { path: "Profile", element: <Profile /> },
+        { path: "SignIn", element: <SignIn /> },
+        { path: "*", element: <Notfound /> },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={routers}></RouterProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainDashboard() {
+  return (
+    <>
+      <h1>MainDashboard</h1>
+    </>
+  );
+}
+
+export default MainDashboard;
